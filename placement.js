0c3be8526be5156aa54b0d4eb4fae8dbdc2bbdbb
xwtoh <55676405+xwtoh@users.noreply.github.com>
2021-11-05 21:58:29 +0800

    Update placement.js

--- placement.js
+++ placement.js
@@ -7,10 +7,47 @@ ViewPlacement = (function() {
 
         var result = cached_function.apply(this, arguments); // use .apply() to call it
 
+        
+        
 setTimeout(function(){ 
-$('html, body').animate({
-    scrollTop: ($('#DataTables_Table_0').first().offset().top)
-},500);}, 1000);
+if (navigator.userAgent.match(/iPad|iPhone|iPod|Android|Windows Phone/i)) {  
+
+            function customScrollTo(to, duration) {
+                var start = 0,
+                    change = to - start,
+                    currentTime = 0,
+                    increment = 20;
+
+                var animateScroll = function(){        
+                    currentTime += increment;
+                    var val = Math.easeInOutQuad(currentTime, start, change, duration);                        
+                    window.scrollTo(0,val);
+
+                    if(currentTime < duration) {
+                        setTimeout(animateScroll, increment);
+                    }
+                };
+                animateScroll();
+            }
+
+            Math.easeInOutQuad = function (t, b, c, d) {
+                t /= d/2;
+                if (t < 1) return c/2*t*t + b;
+                t--;
+                return -c/2 * (t*(t-2) - 1) + b;
+            };
+
+            customScrollTo(($('#DataTables_Table_0').first().offset().top), 1000);
+}else{
+            $('html, body').animate({
+                scrollTop: ($('#DataTables_Table_0').first().offset().top)
+            }, 1000, function(){
+                $('#DataTables_Table_0').clearQueue();
+            });
+}   }, 1000);
+
+   
+        
         return result;
     };
 })();
