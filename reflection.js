091bdd58cab622b27a90cd638fbbc834adea711a
xwtoh <55676405+xwtoh@users.noreply.github.com>
2021-11-19 16:31:18 +0800

    add

---
+++ reflection.js
@@ -0,0 +1,11 @@
+
+
+  $('#frmUpdate2').remove();
+  
+  var what = $('#tblQuestion610').text().split(' ').length;
+  
+  var sowhat = $('#tblQuestion611').text().split(' ').length;
+
+  var nowwhat = $('#tblQuestion612').text().split(' ').length;
+  
+  console.log(what+sowhat+nowwhat);
