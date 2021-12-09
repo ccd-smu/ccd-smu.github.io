bf442af996142ea9a13fb5ec953cbba682dbfbed
xwtoh <55676405+xwtoh@users.noreply.github.com>
2021-12-09 09:39:03 +0800

    updated

--- assessment.js
+++ assessment.js
@@ -1,24 +1,27 @@
 $( document ).ready(function() {
 	
-	 SaveStudentAssessment = (function() {
-    var cached_function = SaveStudentAssessment;
-    return function() {
-        // your code
+	SaveStudentAssessment = (function() {
+   var cached_function = SaveStudentAssessment;
+   return function() {
+	   // your code
 
-        var result = cached_function.apply(this, arguments); // use .apply() to call it
-   setTimeout(function(){ 
-   	location.href = 'https://shib.chalkandwire.com/ep2_smu/FieldPlacementList.aspx?cus=465';
+	   var result = cached_function.apply(this, arguments); // use .apply() to call it
+  setTimeout(function(){ 
+	  location.href = 'https://shib.chalkandwire.com/ep2_smu/FieldPlacementList.aspx?cus=465';
+	  localStorage.setItem('GLOnum', null);
+	  localStorage.setItem('idid', null);
+
 }, 1000);
 
 return result;
-    };
+   };
 })();
 
 var GLOnum2 = localStorage.getItem('GLOnum');
 
-if(GLOnum2 == null){  }
+if(GLOnum2){  }
 else {
-	
+   
 var GLOnum = localStorage.getItem('GLOnum');
 
 var idid = localStorage.getItem('idid');
@@ -27,34 +30,34 @@ if ($('#lblOwner:contains("' + idid + '")'))
 {
 setTimeout(function(){
 $('.ckNotApplicable').attr('checked', true);
-}, 1000);
-	
+}, 1500);
+   
 } else {alert("Student mismatch")};
 
 
 if (GLOnum.includes("5"))
 {setTimeout(function(){
-	$('.txtScore:eq(3) option[value=Exposure]').attr('selected','selected');
-	$('.ckNotApplicable:eq(3)').attr('checked', false);
-}, 1500);};
+   $('.txtScore:eq(3) option[value=Exposure]').attr('selected','selected');
+   $('.ckNotApplicable:eq(3)').attr('checked', false);
+}, 2000);};
 
 if (GLOnum.includes("4"))
 {setTimeout(function(){
-	$('.txtScore:eq(2) option[value=Exposure]').attr('selected','selected');
-	$('.ckNotApplicable:eq(2)').attr('checked', false);
-}, 1500);};
-	
+   $('.txtScore:eq(2) option[value=Exposure]').attr('selected','selected');
+   $('.ckNotApplicable:eq(2)').attr('checked', false);
+}, 2000);};
+   
 if (GLOnum.includes("3"))
 {setTimeout(function(){
-	$('.txtScore:eq(1) option[value=Exposure]').attr('selected','selected');
-	$('.ckNotApplicable:eq(1)').attr('checked', false);
-}, 1500);};
-	
+   $('.txtScore:eq(1) option[value=Exposure]').attr('selected','selected');
+   $('.ckNotApplicable:eq(1)').attr('checked', false);
+}, 2000);};
+   
 if (GLOnum.includes("2"))
 {setTimeout(function(){
-	$('.txtScore:eq(0) option[value=Exposure]').attr('selected','selected');
-	$('.ckNotApplicable:eq(0)').attr('checked', false);
-}, 1500);};
+   $('.txtScore:eq(0) option[value=Exposure]').attr('selected','selected');
+   $('.ckNotApplicable:eq(0)').attr('checked', false);
+}, 2000);};
 
 
 
