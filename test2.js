b9d76dd572d1674ebe8a0e35b2e8cbc9648c1440
xwtoh <55676405+xwtoh@users.noreply.github.com>
2022-03-18 21:55:31 +0800

    mob

--- test2.js
+++ test2.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   if (window.location.href.indexOf("https://shib.chalkandwire.com/ep2_smu/FieldPlacementList.aspx?cus=465") > -1) {
   $('body').append($('<script src="https://xwtoh.github.io/placement-mobile.js"></script>'));
-  $('head').append('<link rel="stylesheet" type="text/css" href="https://xwtoh.github.io/Placement.css">');}
+  $('head').append('<link rel="stylesheet" type="text/css" href="https://xwtoh.github.io/Placement-mobile.css">');}
   
  else if (window.location.href.indexOf("https://shib.chalkandwire.com/ep2_smu/DemographicsRun.aspx?cus=465&TopicId=92&deptID=0") > -1) {
   $('body').append($('<script src="https://xwtoh.github.io/reflection.js"></script>'));
